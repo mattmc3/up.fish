7fc377d5ea1a712891c2306e66196c6a3047b0a6
mattmc3 <mattmc3@gmail.com>
2019-11-14 10:07:33 -0500

    Completion spelling fix

--- functions/upp.fish
+++ functions/upp.fish
@@ -1,4 +1,4 @@
-function upp --description 'Get the path of an ancestory directory'
+function upp --description 'Get the path of an ancestor directory'
     test -n "$argv" || set argv 1
     set pathname $PWD/(string repeat -n "$argv[1]" ../ ^/dev/null)
     realpath $pathname
